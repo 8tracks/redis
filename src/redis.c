764a8ea3b6a277bf860bef55afd014e949d41bd4
Ian Lozinski <ian.lozinski@gmail.com>
2015-02-09 13:03:14 -0800

    Add ability for slaves to expire keys

--- src/redis.c
+++ src/redis.c
@@ -935,7 +935,7 @@ void clientsCron(void) {
 void databasesCron(void) {
     /* Expire keys by random sampling. Not required for slaves
      * as master will synthesize DELs for us. */
-    if (server.active_expire_enabled && server.masterhost == NULL)
+    if (server.active_expire_enabled)
         activeExpireCycle(ACTIVE_EXPIRE_CYCLE_SLOW);
 
     /* Perform hash tables rehashing if needed, but only if there are no
